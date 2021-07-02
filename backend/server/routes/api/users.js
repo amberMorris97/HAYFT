5f5ecbd51233e5489aea07de685dcc2c3f40abb7
Amber Morris <ambermorris1997@gmail.comgit config --global core.editor vimgit config --global core.autocrlf inputgit config --global user.email ambermorris1997@gmail.com>
2021-07-02 12:58:20 -0400

    user update and delete endpoints created

--- backend/server/routes/api/users.js
+++ backend/server/routes/api/users.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 const passport = require('passport');
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 const auth = require('../auth');
 const Users = mongoose.model('Users');
 const db = require('../../../database/index');
 
 router.post('/register', auth.optional, (req, res, next) => {
-  const { user } = req.body;
+  const { name, username, email, password } = req.body;
 
-  if (!user.email) {
+  if (!email) {
     return res.status(422).send({
       errors: {
         email: 'is required',
@@ -16,7 +17,7 @@ router.post('/register', auth.optional, (req, res, next) => {
     });
   }
 
-  if (!user.password) {
+  if (!password) {
     return res.status(422).send({
       errors: {
         password: 'is required',
@@ -24,7 +25,7 @@ router.post('/register', auth.optional, (req, res, next) => {
     });
   }
 
-  if (!user.name) {
+  if (!name) {
     return res.status(422).send({
       errors: {
         name: 'is required',
@@ -32,7 +33,7 @@ router.post('/register', auth.optional, (req, res, next) => {
     });
   }
 
-  if (!user.username) {
+  if (!username) {
     return res.status(422).send({
       errors: {
         username: 'is required',
@@ -40,11 +41,11 @@ router.post('/register', auth.optional, (req, res, next) => {
     });
   }
 
+  const user = { name, email, username, password };
+
   const finalUser = new Users(user);
 
-  console.log(finalUser, 'FINAL U')
-
-  finalUser.setPassword(user.password);
+  finalUser.setPassword(password);
 
   return finalUser.save()
     .then(() => res.send({ user: finalUser.toAuthJSON() }));
@@ -105,6 +106,34 @@ router.get('/current', async (req, res, next) => {
     res.sendStatus(400);
   }
 
+});
+
+router.patch('/updateUser/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, email, username, password } = req.body;
+  let filter;
+  if (name) filter = { name };
+  if (email) filter = { email };
+  if (password) {
+    bcrypt.genSalt(10, async (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hashed) => {
+        const updated = await Users.updateOne({ _id:mongoose.Types.ObjectId(`${id}`) }, { password: hashed });
+        if (updated) return res.status(200).send('Information updated');
+        if (!updated) return res.status(400).send('Information could not be updated at this time');
+      })
+     });
+     return;
+  }
+  const updated = await Users.updateOne({_id:mongoose.Types.ObjectId(`${id}`)}, filter)
+  if (updated) res.status(200).send('Information updated');
+  if (!updated) res.status(400).send('Information could not be updated at this time');
+});
+
+router.put('/deleteUser/:id', async (req, res) => {
+  const { id }  = req.params;
+  const deleted = await Users.deleteOne({ _id:mongoose.Types.ObjectId(`${id}`) });
+  if (deleted) res.send('User has been deleted');
+  if (!deleted) res.send('User could not be deleted at this time');
 });
 
 module.exports = router;
