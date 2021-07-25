70d68fd0048d222f6c47df7ff6f359575dc69f33
Amber Morris <ambermorris1997@gmail.comgit config --global core.editor vimgit config --global core.autocrlf inputgit config --global user.email ambermorris1997@gmail.com>
2021-07-25 03:37:12 -0400

    created fetch and update routes for blog posts

--- backend/server/routes/api/blogs.js
+++ backend/server/routes/api/blogs.js
@@ -3,7 +3,15 @@ const router = require('express').Router();
 const Blogs = require('../../../database/models/blogs');
 const db = require('../../../database/index');
 
-router.post('/newBlog', async (req, res) => {
+router.get('/posts', async (req, res) => {
+  const blogPosts = await Blogs.find({});
+
+  if (blogPosts) return res.send(blogPosts);
+
+  return res.status(400).send('No blog posts found.');
+});
+
+router.post('/newPost', async (req, res) => {
   const data = req.body;
   const newPost = new Blogs(data);
 
@@ -14,7 +22,7 @@ router.post('/newBlog', async (req, res) => {
   return res.status(400).send('Unable to post at this time.');
 });
 
-router.put('/deleteBlog/:id', async (req, res) => {
+router.put('/deletePost/:id', async (req, res) => {
   const { id } = req.params;
 
   const deleted = await Blogs.deleteOne({ _id:mongoose.Types.ObjectId(`${id}`) });
@@ -22,6 +30,24 @@ router.put('/deleteBlog/:id', async (req, res) => {
   if (deleted) return res.send(deleted);
 
   return res.status(400).send('Post cannot be deleted at this time.');
+});
+
+router.patch('/updatePost/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, post, img } = req.body;
+
+  const exisitingBlogPost = await Blogs.findOne({ _id:mongoose.Types.ObjectId(`${id}`) });
+
+  if (!exisitingBlogPost) return res.status(400).send('Post does not exist');
+
+  if (title) exisitingBlogPost.title = title;
+  if (post) exisitingBlogPost.post = post;
+  if (img) exisitingBlogPost.img = img;
+
+  const updatedBlog = await exisitingBlogPost.save();
+
+  if (updatedBlog) return res.send('Post updated successfully.');
+  return res.status(400).send('Post cannot be updated at this time.');
 });
 
 module.exports = router;
