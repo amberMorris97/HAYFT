bc62fac0fc4824fa1ae728ad9b755ecdda63a307
Amber Morris <ambermorris1997@gmail.comgit config --global core.editor vimgit config --global core.autocrlf inputgit config --global user.email ambermorris1997@gmail.com>
2021-08-27 07:31:49 -0400

    styling...

--- frontend/src/components/Home/About/About.jsx
+++ frontend/src/components/Home/About/About.jsx
@@ -22,9 +22,9 @@ const About = () => {
       <Card style={imageStyle} variant="elevation" elevation={24}>
           <img src="./images/jrselfie.jpeg" width="750" height="550" alt="photo-of-jane" />
       </Card>
-      <Card style={textStyle} variant="elevation" elevation={2}>
-        <p className="openSans about-para">Jane has spent her career developing corporate DEI strategies anchored in social justice foundations. As the first Head of DEI (CDO) at W.L. Gore &amp; Associates (www.gore.com) Jane developed a philosophy and strategy that built on Gore’s humanistic values and innovative business model. Relying on culture change and social movement methodologies, Jane galvanized the involvement of thousands of Associates from around the world. Using curiosity and conversation, teams deepened their understanding of identity differences and committed to disrupting bias in day-to-day life. With a strong focus on systems and structures, Jane&apos;s approach is rooted in her belief that empathy and dialogue can solve most things.</p>
-      </Card>
+
+        <p style={{ color: 'black', width: '75%' }} className="openSans about-para">Jane has spent her career developing corporate DEI strategies anchored in social justice foundations. As the first Head of DEI (CDO) at W.L. Gore &amp; Associates (www.gore.com) Jane developed a philosophy and strategy that built on Gore’s humanistic values and innovative business model. Relying on culture change and social movement methodologies, Jane galvanized the involvement of thousands of Associates from around the world. Using curiosity and conversation, teams deepened their understanding of identity differences and committed to disrupting bias in day-to-day life. With a strong focus on systems and structures, Jane&apos;s approach is rooted in her belief that empathy and dialogue can solve most things.</p>
+
       <Link style={{ textDecoration: 'none' }} to="/contact"><PurpleButton text={'WORK WITH ME'} /></Link>
     </div>
   );
